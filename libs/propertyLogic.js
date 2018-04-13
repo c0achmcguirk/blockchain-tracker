fe73cb8f15ef1252eaccc2df4dead7f2ac4d8044
Paul <gitkraken@paultastic.com>
2018-04-13 12:08:14 -0500

    Adds propertyLogic and admin.js files for a start

---
+++ libs/propertyLogic.js
@@ -0,0 +1,32 @@
+/**
+ * Node module that will perform property-related actions like:
+ *   - Register a property (real-estate)
+ *   - Update a property
+ *   - Merge a property
+ *   - Look up a property
+ */
+
+module.exports = class PropertyLogic {
+  /**
+   * Save a property to the blockchain. It will handle the initial save and the case where you
+   * are updating an existing property.
+   * @param {string} id The ID of the property. If the property doesn't have an id, leave this as
+   *   null or undefined.
+   * @param {Object[]} coordinates The GPS coordinates of the property. Only support 4 points now.
+   * @param {string} name The name of the property for displaying on a UI.
+   * @returns {Property} The updated property.
+   */
+  save(id, coordinates, name) {
+    //no-op
+  }
+
+  /**
+   * Look up a property on the blockchain based on GPS coordinates (one point)
+   * @param {Coordinate} coordinate The GPS coordinates of a point on the map.
+   * @returns {Property} The property belonging to that coordinate, or undefined if we don't have
+   *   a property belonging to that point.
+   */
+  getPropertyByCoordinate(coordinate) {
+    return undefined;
+  }
+};
