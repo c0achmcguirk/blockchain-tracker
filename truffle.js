0734983f50fe89a2b98696f36efbe5d82d8f36ae
Kyle Laughlin <kyle.laughlin@paymentspring.com>
2018-04-13 10:52:34 -0500

    add development config

--- truffle.js
+++ truffle.js
@@ -1,4 +1,9 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  development: {
+    host: "127.0.0.1",
+    port: 7545,
+    network_id: "*" // match any network
+  }
 };
