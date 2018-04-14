096789f450fc13d02df29a4c6a54ea8593e3bbb5
Kyle Laughlin <kyle.laughlin@paymentspring.com>
2018-04-14 13:53:10 -0500

    add calls for get property functions

--- libs/propertyLogic.js
+++ libs/propertyLogic.js
@@ -16,6 +16,7 @@ var blockchainConfig = config.get('Blockchain');
 var contractBin      = config.get('TestContract.bin');
 var contractAbi      = config.get('TestContract.abi');
 var fromAddress      = config.get('fromAddress');
+var fixnumFactor     = config.get('fixnumFactor');
 
 class PropertyLogic {
 
@@ -31,40 +32,6 @@ class PropertyLogic {
       let url = `http://${ blockchainConfig.host }:${ blockchainConfig.port }`;
       web3 = new Web3(new Web3.providers.HttpProvider(url));
     }
-  }
-
-  /**
-   * Save a property to the blockchain. It will handle the initial save and the case where you
-   * are updating an existing property.
-   * @param {string} id The ID of the property. If the property doesn't have an id, leave this as
-   *   null or undefined.
-   * @param {Object[]} coordinates The GPS coordinates of the property. Only support 4 points now.
-   * @param {string} name The name of the property for displaying on a UI.
-   * @returns {Property} The updated property.
-   */
-  save(id, coordinates, name) {
-    //no-op
-  }
-
-  /**
-   * Look up a property on the blockchain based on GPS coordinates (one point)
-   * @param {Coordinate} coordinate The GPS coordinates of a point on the map.
-   * @returns {Property} The property belonging to that coordinate, or undefined if we don't have
-   *   a property belonging to that point.
-   */
-  getPropertyByCoordinate(coordinate) {
-    return undefined;
-  }
-
-	/**
-   * Look up a property by the id (address) on the blockchain, returning it.
-   * @param {string} id The property's unique Id, which is the contract address
-   * @returns {Promise<Property>} The property belong to that id, or undefined if we don't have it.
-   */
-	getPropertyById(id) {
-    // return testingContractInstance.methods.getPropertyById(id).call({from: fromAddress, gas: 5000000}).then((result) => {
-    //   resolve(result);
-    // });
   }
 
   /**
@@ -87,6 +54,56 @@ class PropertyLogic {
         });
       }
     });
+  }
+
+  /**
+   * Save a property to the blockchain. It will handle the initial save and the case where you
+   * are updating an existing property.
+   * @param {string} id The ID of the property. If the property doesn't have an id, leave this as
+   *   null or undefined.
+   * @param {Object[]} coordinates The GPS coordinates of the property. Only support 4 points now.
+   * @param {string} name The name of the property for displaying on a UI.
+   * @returns {Property} The updated property.
+   */
+  saveProperty(id, coordinates, name) {
+    //no-op
+  }
+
+  /**
+   * Look up a property on the blockchain based on GPS coordinates (one point)
+   * @param {Coordinate} coordinate The GPS coordinates of a point on the map.
+   * @returns {Property} The property belonging to that coordinate, or undefined if we don't have
+   *   a property belonging to that point.
+   */
+  getPropertyByCoordinate(latitude, longitude) {
+    let promise = new Promise((resolve, reject) => {
+      this.getContractInstance()
+      .then((newContractInstance) => {
+        newContractInstance.methods.getPropertyAt(latitude, longitude).call({from: fromAddress, gas: 500000}).then((result) => {
+          resolve(result);
+        });
+      });
+    });
+
+    return promise;
+  }
+
+	/**
+   * Look up a property by the id (address) on the blockchain, returning it.
+   * @param {string} id The property's unique Id, which is the contract address
+   * @returns {Promise<Property>} The property belong to that id, or undefined if we don't have it.
+   */
+	getPropertyById(id) {
+    let promise = new Promise((resolve, reject) => {
+      this.getContractInstance()
+      .then(function(newContractInstance) {
+        newContractInstance.methods.getPropertyById(id).call({from: fromAddress, gas: 5000000}).then((result) => {
+          resolve(result);
+        });
+      });
+    });
+
+    return promise;
   }
 
   /**
--- test/test_propertyLogic.js
+++ test/test_propertyLogic.js
@@ -5,6 +5,41 @@ describe("PropertyLogic", () => {
 
   var mockContract = {
     methods: {
+      getPropertyById() {
+        return {
+          call() {
+            return new Promise((resolve, reject) => {
+              resolve({
+                id: '123abc',
+                name: 'Creamery Building',
+                owner: 'Real Building, LLC',
+                address: '701 P St, Lincoln, NE 68502',
+                top_long: '40.814649',
+                left_lat: '-96.711242',
+                bottom_long: '40.814239',
+                right_lat: '-96.710925',
+              });
+            });
+          }
+        };
+      },
+      getPropertyAt() {
+        return {
+          call() {
+            return new Promise((resolve, reject) => {
+              resolve({
+                id: '789xyz',
+                owner: 'Joe Smith',
+                address: '',
+                top_long: '40.814651',
+                left_lat: '-96.709817',
+                bottom_long: '40.814248',
+                right_lat: '-96.709621'
+              })
+            });
+          }
+        };
+      },
       sayHello() {
         return {
           call() {
@@ -39,14 +74,40 @@ describe("PropertyLogic", () => {
     var someVal = "foo";
 		var propertyLogic = new PropertyLogic;
 
-    xdescribe('#getPropertyById', () => {
-      it("should return undefined if property id doesn't exist", () => {
-        let propertyID = 'bogusID';
-        return propertyLogic.getPropertyById(propertyID).then((result) => {
+    describe('#getPropertyById', () => {
+      xit("should return undefined if property id doesn't exist", () => {
+        let propertyId = 'bogusID';
+        return propertyLogic.getPropertyById(propertyId).then((result) => {
           assert.strictEqual(undefined, result);
         });
       });
+
+      it("should return a property if property is found", () => {
+        propertyLogic._contractInstance = mockContract;
+        let propertyId = "123abc";
+        return propertyLogic.getPropertyById(propertyId).then((result) => {
+          assert.strictEqual("123abc", result.id);
+          assert.strictEqual("Creamery Building", result.name);
+          assert.strictEqual("Real Building, LLC", result.owner);
+          assert.strictEqual("701 P St, Lincoln, NE 68502", result.address);
+          assert.strictEqual("40.814649", result.top_long);
+          assert.strictEqual("-96.711242", result.left_lat);
+          assert.strictEqual("40.814239", result.bottom_long);
+          assert.strictEqual("-96.710925", result.right_lat);
+        })
+      })
     });
+
+    describe('#getPropertyByCoordinate', () => {
+      it("should return a property if property is found", () => {
+        propertyLogic._contractInstance = mockContract;
+        let lat = '40.814551';
+        let long = '-96.709717';
+        return propertyLogic.getPropertyByCoordinate(lat, long).then((result) => {
+          assert.strictEqual("789xyz", result.id);
+        })
+      })
+    })
 
     describe('#getHelloFromContract', () => {
       it("should return 'Hello from contract' when talking to the contract", () => {
@@ -60,6 +121,7 @@ describe("PropertyLogic", () => {
 
     describe('#addToCountFromTestingContract', () => {
       it("should return 1 when adding to the count", () => {
+        propertyLogic._contractInstance = mockContract;
         return propertyLogic.addToCountFromTestingContract().then((result) => {
           assert.strictEqual('1', result);
         });
